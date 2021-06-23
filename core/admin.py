54cb7d6ae19b46c3b513b92fe3afa90df146abd7
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-06-24 00:20:59 +0100

    Added admin view

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Post, Image, Comment
 
-# Register your models here.
+
+class ImageAdminInline(admin.StackedInline):
+    model = Image
+    extra = 1
+    classes = ['collapse']
+
+
+class CommentAdminInline(admin.StackedInline):
+    model = Comment
+    readonly_fields = ('date', 'up_vote', 'down_vote',)
+    extra = 1
+    classes = ['collapse']
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = (ImageAdminInline, CommentAdminInline)
+    readonly_fields = ('date',)
+    fields = ('category', 'date', 'title', 'description',)
+    list_display = ('title', 'category', 'date',)
+    ordering = ('-date',)
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
