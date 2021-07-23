ed54da21a11c06610cc329b5b77dcc7ac2a0162a
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2021-07-23 22:58:02 +0100

    Amended database settings in settings.py

--- equestrian_forum/settings.py
+++ equestrian_forum/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'equestrian_forum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
